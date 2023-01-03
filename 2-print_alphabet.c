30ab1bf46f168146d32f47c9b308b9f2f475b513
ssenayon <oreseton@yahoo.com>
2023-01-02 16:09:34 -0800

    alpha

---
+++ 2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char did;
+	for (did = a; did <= z; did++)
+		putchar (did);
+		
+	putchar('\n')
+		return(0)
+}
