7f4fc233bea24229a7e21af16b8e628b5f5f3d51
ssenayon <oreseton@yahoo.com>
2023-01-03 04:55:28 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - backward alphabets
+ *
+ * Return:  Always 0 (success)
+ */
+int main(void)
+{
+	char n;
+	for (char = 'z'; char >= 'a'; char--)
+		putchar ("%d", n);
+	putchar ('\n');
+	return (0);
+}
