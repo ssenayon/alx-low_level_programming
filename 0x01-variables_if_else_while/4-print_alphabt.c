c5246820407fd84653b87a938de27e4b8cd9bfb4
ssenayon <oreseton@yahoo.com>
2023-01-02 19:06:44 -0800

    wow

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - exclusion
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			if ( alpha != 'e' && alpha != 'q')
+				putchar (alpha);
+		}
+	putchar ('\n');
+	return (0);
+}
