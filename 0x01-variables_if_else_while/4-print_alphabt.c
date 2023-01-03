3e5919308f61fe94e02bae801cf20aab0e074435
ssenayon <oreseton@yahoo.com>
2023-01-02 19:11:33 -0800

    lol

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
-			if ( alpha != 'e' && alpha != 'q')
+			if (alpha != 'e' && alpha != 'q')
 				putchar (alpha);
 		}
 	putchar ('\n');
